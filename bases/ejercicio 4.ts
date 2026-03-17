8d720fe71c42539539d60afb3fc1c460a5342504
Edisbel Camargo <lebside2701@gmail.com>
2026-03-17 14:08:47 -0500

    Agregando Ejercicion 4 de interfaces y objetos

---
+++ bases/ejercicio 4.ts
@@ -0,0 +1,85 @@
+// Crear interfaces
+
+// Cree una interfaz para validar el auto (el valor enviado por parametro)
+interface Auto{
+  encender:boolean,
+  velocidadMaxima:number,
+  acelerar():void,
+}
+const conducirBatimovil = ( auto:Auto ):void => {
+  auto.encender = true;
+  auto.velocidadMaxima = 100;
+  auto.acelerar();
+}
+
+const batimovil:Auto = {
+  encender:false,
+  velocidadMaxima:0,
+  acelerar(){
+    console.log("...... gogogo!!!");
+  }
+}
+
+// Cree una interfaz con que permita utilzar el siguiente objeto
+// utilizando propiedades opcionales
+interface Guason{
+  reir?:boolean,
+  comer?:boolean,
+  llorar?:boolean,
+}
+const guason:Guason = {
+  reir: true,
+  comer:true,
+  llorar:false
+}
+
+const reir = ( guason:Guason ):void => {
+  if( guason.reir ){
+    console.log("JAJAJAJA");
+  }
+}
+
+
+// Cree una interfaz para la siguiente funcion
+interface ciudad{
+  (ciudades:string[]):number,
+}
+
+const ciudadGotica:ciudad = ( ciudadanos:string[] ):number => {
+  return ciudadanos.length;
+}
+
+// Cree una interfaz que obligue crear una clase
+// con las siguientes propiedades y metodos
+interface info{
+  nombre:string,
+  edad:number;
+  sexo:string,
+  estadoCivil:string,
+  imprimirBio():void,
+}
+/*
+  propiedades:
+    - nombre
+    - edad
+    - sexo
+    - estadoCivil
+    - imprimirBio(): void // en consola una breve descripcion.
+*/
+class Persona implements info{
+  public nombre:string;
+  public edad:number;
+  public sexo:string;
+  public estadoCivil:string;
+
+  constructor(nombre:string, edad:number, sexo:string, estadoCivil:string){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+    this.estadoCivil = estadoCivil;
+  }
+
+  imprimirBio(){
+    
+  }
+}
