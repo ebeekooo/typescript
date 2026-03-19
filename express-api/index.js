852a28d81dbd6cc322cd5646008e2c1cddcc68bd
Edisbel Camargo <lebside2701@gmail.com>
2026-03-19 09:42:21 -0500

    Creando nueva carpeta para el uso de ExpressJs como ejemplo de prueba

---
+++ express-api/index.js
@@ -0,0 +1,16 @@
+//Web server
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+    msg:'Todo salió bien'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
